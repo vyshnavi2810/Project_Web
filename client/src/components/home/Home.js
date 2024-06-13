128db59972efaa98712838106ed5194faa52d651
vyshnavi2810 <vaishnaviadepu2004@gmail.com>
2024-06-14 00:29:25 +0530

    Update Home.js

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,23 +1,13 @@
 import "./Home.css";
-import articleImage from "../../assets/logo-1.jpeg";
+import articleImage from "../../assets/Article-Writing-1.jpg";
 
 function Home() {
   return (
     <div className='articleHome'>
-      <h1 style={{color:'var(--crimson)'}}> Ocean of Knowledge</h1>
+      <h1 style={{color:'#4e5180'}}> Ocean of Knowledge</h1>
       <img src={articleImage} alt="" className="artcleImage" />
       <p className="lead">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officia
-        explicabo voluptatum excepturi dignissimos dolorum iste sed culpa nulla
-        ab doloremque error placeat pariatur, similique facere soluta impedit
-        inventore nam voluptas eum, laboriosam ipsam minus voluptatibus quaerat!
-        Consequatur animi exercitationem accusantium repudiandae error
-        voluptate, eveniet adipisci, fuga nostrum eligendi a harum! Atque, nihil
-        eligendi labore, omnis, deleniti dolor est rem vitae id doloribus
-        tempora distinctio excepturi cumque architecto. Repudiandae amet dolor
-        voluptatibus ad in repellendus velit et, inventore, quo corrupti optio
-        ab eum recusandae nisi error aut omnis. Fugiat dolore vero magni ea
-        quibusdam commodi rem dolor repellendus. Voluptatem, assumenda.
+      Welcome to Ocean of Knowledge, where learning knows no bounds! Ocean of Knowledge is a web application crafted to immerse you in the vast sea of education. Perfect for students, professionals, and curious minds alike, this platform offers a plethora of features including personalized study plans, interactive quizzes, and detailed progress tracking. Navigate through a wealth of resources, connect with fellow learners, and explore the depths of knowledge with ease. Let Ocean of Knowledge be your compass on your educational journey, guiding you to new horizons of understanding and mastery. Dive in and discover the endless possibilities that await!
       </p>
     </div>
   );
